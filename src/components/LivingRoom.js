1952199d4e2ac6b07eb0a28188bfb99c024f94f8
Defne Celikoyar <defne.celikoyar@gmail.com>
2022-01-05 11:16:14 -0500

    defne 11am jan5

---
+++ src/components/LivingRoom.js
@@ -0,0 +1,34 @@
+import React, { useState }from "react";
+import { useMousePosition } from "./useMousePosition";
+
+import idleLivingRoom from "../images/IdleLivingRoom.gif";
+import tvLivingRoom from "../images/TVLivingRoom.gif";
+import pixelDiary from "../images/Pixel_diary.png";
+
+function LivingRoom() {
+    const [livingRoomAnimation, livingRoomAnimation] = useState(idleLivingRoom)
+  
+    const position = useMousePosition();
+    const x = position.x;
+    const y = position.y;
+  
+    function handleSwitch() {
+      if (x < 832 && y < 457 && x > 612 && y > 285) {
+        livingRoomAnimation(idleLivingRoom);
+      } else if (x < 1090 && y < 770 && x > 716 && y > 623) {
+        livingRoomAnimation(tvLivingRoom);
+      } else if (x < 1376 && y < 541 && x > 1182 && y > 366) {
+        setLivingRoomAnimation(pixelDiary);
+      } 
+
+    return (
+      <div style={{backgroundColor: darkMode ? "#2E282A" : "white"}}>
+        <div>
+          {x}:{y}
+        </div>
+        <img src={bedroomAnimation} alt="bedroom" className="bedroom-image" onClick={handleSwitch}/>
+      </div>
+    );
+  }
+  
+  export default LivingRoom;
