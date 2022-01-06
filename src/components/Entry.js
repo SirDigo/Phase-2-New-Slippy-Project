91f9921d451f1ad394b22dad52e431ff6493a3e0
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-01-05 16:32:23 -0800

    journal and garden updates

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 
-function Entry ({entry}){
+function Entry ({entry, handleDeleteEntry }){
+
+  function deleteEntry () {
+    handleDeleteEntry(entry)
+  }
+
+
 console.log(entry)
   return (
     <tr className="table-content">
     <td>{entry.date}</td>
     <td>{entry.entry}</td>
-    <td>
+    <td><button type="button" onClick={deleteEntry} style={{ background:"none", fontFamily:"cursive", borderRadius:"60px", cursor:"pointer", boxShadow: "0 0 5px #2ba805"  }}>Remove from Journal</button>
     </td>
   </tr>
 );
--- src/components/Garden.js
+++ src/components/Garden.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMousePosition } from "./useMousePosition";
+
+// import idleGarden from "../images/IdleGarden.gif";
+// import placeHolder from "../images/placeHolder.gif";
+// import placeHolder from "../images/placeHolder.gif";
 
 function Garden() {
 
-    return (
-        <div>
-            HEllo
-        </div>
-    )
+    // const [gardenAnimation, setGardenAnimation] = useState(idleGarden)
+
+    // const position = useMousePosition();
+    // const x = position.x;
+    // const y = position.y;
+
+    //  let history = useHistory();
+
+    // function handleSwitch() {
+    //     if (x < 832 && y < 457 && x > 612 && y > 285) {
+    //         setGardenAnimation(placeHolder);
+    //     } else if (x < 1090 && y < 770 && x > 716 && y > 623) {
+    //         setGardenAnimation(placeHolder);
+    //     } else if (x < 1112 && y < 970 && x > 796 && y > 892 {
+    //          history.push("/livingroom")
+    //     } 
+    //   }
+
+    // return (
+    //     <div>
+    //     <div>
+    //       {x}:{y}
+    //     </div>
+    //     <img src={gardenAnimation} alt="garden" className="garden-image" onClick={handleSwitch}/>
+    //   </div>
+    // )
 }
 
 export default Garden;
