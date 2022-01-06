feb876ef1fbb8170f9c34f173a11f38d9ac0f7a2
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-01-06 08:59:20 -0800

    adding garden pictures and db.json

--- src/components/Garden.js
+++ src/components/Garden.js
@@ -1,38 +1,39 @@
 import React, { useState } from "react";
 import { useMousePosition } from "./useMousePosition";
-
-// import idleGarden from "../images/IdleGarden.gif";
-// import placeHolder from "../images/placeHolder.gif";
-// import placeHolder from "../images/placeHolder.gif";
+import { useHistory } from "react-router-dom";
+import idleGarden from "../images/IdleGarden.gif";
+import tanningGarden from "../images/TanningGarden.gif";
+import wateringGarden from "../images/WateringGarden.gif";
 
 function Garden() {
 
-    // const [gardenAnimation, setGardenAnimation] = useState(idleGarden)
+    const [gardenAnimation, setGardenAnimation] = useState(idleGarden)
 
-    // const position = useMousePosition();
-    // const x = position.x;
-    // const y = position.y;
+    const position = useMousePosition();
+    const x = position.x;
+    const y = position.y;
 
-    //  let history = useHistory();
+     let history = useHistory();
 
-    // function handleSwitch() {
-    //     if (x < 1376 && y < 541 && x > 1182 && y > 366) {
-    //         setLivingRoomAnimation(blanketGarden);
-    //     } else if (x < 1090 && y < 770 && x > 716 && y > 623) {
-    //         setGardenAnimation(wateringGarden);
-    //     } else if (x < 1107 && y < 357 && x > 930 && y > 295 {
-    //          history.push("/livingroom")
-    //     } 
-    //   }
-
-    // return (
-    //     <div>
-    //     <div>
-    //       {x}:{y}
-    //     </div>
-    //     <img src={gardenAnimation} alt="garden" className="garden-image" onClick={handleSwitch}/>
-    //   </div>
-    // )
+        function handleSwitch() {
+            if (x < 1376 && y < 541 && x > 1182 && y > 366) {
+            setGardenAnimation(tanningGarden);
+            } else if (x < 1090 && y < 770 && x > 716 && y > 623) {
+            setGardenAnimation(wateringGarden);
+            } else if (x < 1130 && y < 606 && x > 860 && y > 544) {
+            setGardenAnimation(idleGarden);
+            } else if (x < 1107 && y < 357 && x > 930 && y > 295) {
+             history.push("/livingroom");          
+        }
+    }
+    return (
+        <div>
+        <div>
+          {x}:{y}
+        </div>
+        <img src={gardenAnimation} alt="garden" className="bedroom-image" onClick={handleSwitch}/>
+      </div>
+    )
 }
 
 export default Garden;
