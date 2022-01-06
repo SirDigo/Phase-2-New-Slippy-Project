0cd61f9ee102043603d8e9ed36e389a635197d42
Defne Celikoyar <defne.celikoyar@gmail.com>
2022-01-06 14:43:57 -0500

    header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 function Header() {
+
+  const myStyle = {
+    backgroundColor: "#FFE799",
+  }
+
   return (
-    <div>
-        <h1>sleepy</h1>
+      <div className="navbar" style={{backgroundColor: "#FFD447"}}>
+        <li style={myStyle}><a style={{color: "#372F32"}} href="/about">About</a></li>
+        <li style={myStyle}><a style={{color: "#372F32"}} href="/bedroom">Bedroom</a></li>
+        <li style={myStyle}><a style={{color: "#372F32"}} href="/livingroom">Living room</a></li>
+        <li style={myStyle}><a style={{color: "#372F32"}} href="/journal">Journal</a></li>
     </div>
   );
 }
